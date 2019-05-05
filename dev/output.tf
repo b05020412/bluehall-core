cfddd8d0d499ec7d299bf49900a466edd9d84630
Hee Won Kim <hkim@Hees-MacBook-Pro.local>
2019-05-05 15:52:37 +0900

    final output

--- dev/output.tf
+++ dev/output.tf
@@ -1,7 +1,27 @@
-output "bastion_host_dns" {
-  value = "${aws_instance.bastion.public_dns}"
-}
+ output "vpc_id" {
+   value = "${module.vpc.vpc_id}"
+ }
 
-output "bastion_host_ip" {
-  value = "${aws_instance.bastion.public_ip}"
-}
+ output "vpc_cidr" {
+   value = "${module.vpc.vpc_cidr}"
+ }
+
+ output "public_subnet_ids" {
+   value = "${module.vpc.public_subnet_ids}"
+ }
+
+ output "private_subnet_ids" {
+   value = "${module.vpc.private_subnet_ids}"
+ }
+
+ output "public_route_table_id" {
+   value = "${module.vpc.public_route_table_id}"
+ }
+
+ output "private_route_table_id" {
+   value = "${module.vpc.private_route_table_id}"
+ }
+
+ output "default_security_group_id" {
+   value = "${module.vpc.default_security_group_id}"
+ }
