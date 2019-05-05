4973b4cf3a0eb7d283536625d692d42ce37b5bb3
Hee Won Kim <hkim@Hees-MacBook-Pro.local>
2019-05-05 15:51:00 +0900

    updated output for bastion

--- dev/output.tf
+++ dev/output.tf
@@ -1,27 +1,7 @@
-output "vpc_id" {
-  value = "${module.vpc.aws_vpc.environment.id}"
+output "bastion_host_dns" {
+  value = "${aws_instance.bastion.public_dns}"
 }
 
-output "vpc_cidr" {
-  value = "${module.vpc.aws_vpc.environment.cidr_block}"
-}
-
-output "public_subnet_ids" {
-  value = "${module.vpc.aws_subnet.public.*.id}"
-}
-
-output "private_subnet_ids" {
-  value = "${module.vpc.aws_subnet.private.*.id}"
-}
-
-output "public_route_table_id" {
-  value = "${module.vpc.aws_route_table.public.id}"
-}
-
-output "private_route_table_id" {
-  value = "${module.vpc.aws_route_table.private.*.id}"
-}
-
-output "default_security_group_id" {
-  value = "${module.vpc.aws_vpc.environment.default_security_group_id}"
+output "bastion_host_ip" {
+  value = "${aws_instance.bastion.public_ip}"
 }
