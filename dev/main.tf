5827026c16f1f4d4e08edf3aa4f11d83fe93a44f
Hee Won Kim <hkim@Hees-MacBook-Pro.local>
2019-05-30 01:43:40 +0900

    updated

--- dev/main.tf
+++ dev/main.tf
@@ -10,6 +10,6 @@ module "vpc" {
   environment = "${var.environment}"
 
   vpc_cidr = "${var.vpc_cidr}"
-  public_subnets = ${var.public_subnets}
-  private_subnets = ${var.private_subnets}
+  public_subnets = "${var.public_subnets}"
+  private_subnets = "${var.private_subnets}"
 }
